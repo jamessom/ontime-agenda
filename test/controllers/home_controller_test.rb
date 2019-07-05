2a3fddde2cf52851371ea839e4101abf7091fa2e
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2019-07-05 01:15:07 -0300

    Adição de página home Criação de uma página básica para evitar a página padrão do Rails. Adição de logos.

---
+++ test/controllers/home_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class HomeControllerTest < ActionDispatch::IntegrationTest
+  test "routes validate" do
+    assert_recognizes({controller: "home", action: "index"}, {path: "/", method: :get})
+    assert_generates "/home/index", controller: "home", action: "index"
+  end
+end
