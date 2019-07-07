164fff1851700c189e237633a9c7f1f5cd7057c0
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2019-07-06 21:32:49 -0300

    Ajuste no teste de url da Home

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  # test "routes validate" do
-  #   assert_recognizes({controller: "home", action: "index"}, {path: "/", method: :get})
-  #   assert_generates "/home/index", controller: "home", action: "index"
-  # end
+  test "check home routes" do
+    assert_generates "/home/index", controller: "home", action: "index"
+  end
 end
